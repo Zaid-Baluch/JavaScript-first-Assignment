56bd3fd43c6390036a9aa04cdd1b4cf8599fa3f4
Zaid <zaidbeluch@gmail.com>
2025-07-30 01:37:53 +0500

    Completed-Chap-1

---
+++ index.js
@@ -0,0 +1,17 @@
+//  CHAPTER 1 = ALERTS
+
+// QUESTION 1
+alert("hello...! welcome to my website");
+
+// QUESTION 2
+alert("Error! please enter valid password");
+
+// QUESTION 3
+alert("Welcome to JS Land... Happy Coding!");
+
+//QUESTION 4
+alert("Welcome to JS Land...");
+alert("Happy Coding! ");
+
+//QUESTION 5
+alert("Hello... I can run JS through my web browser's console");
